f1adaf0300b4f4a7c45f30d20f92b269f45103b6
Jonathan McGlone <jmcglone@umich.edu>
2016-05-14 23:01:10 -0400

    merge berman branding
    new _brand include and cleanup

--- js/init.js
+++ js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $("#button--get-updates").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#get-updates").offset().top
+    }, 2000);
+});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
